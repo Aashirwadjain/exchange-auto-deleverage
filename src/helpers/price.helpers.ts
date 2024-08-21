728bad6ab471ea7edf55995bc3b3d13b4da9d88c
Aashirwad Jain <aashirwad2001jain@gmail.com>
2024-08-21 09:40:32 +0530

    updates: price helpers

--- src/helpers/price.helpers.ts
+++ src/helpers/price.helpers.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const getPrice = async () => {
-    const price = await axios.get("http://54.180.247.27:8082/prices/tickers");
+    const res = await axios.get("http://54.180.247.27:8082/prices/tickers");
+    const price = res.data;
     console.log("price: ", price);
 }
