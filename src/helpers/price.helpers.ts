319feb845cde32e3c579c80c93c4a7c7bdfdffd2
Aashirwad Jain <aashirwad2001jain@gmail.com>
2024-08-27 09:55:54 +0530

    updates: price and datastore helpers

--- src/helpers/price.helpers.ts
+++ src/helpers/price.helpers.ts
@@ -1,7 +1,35 @@
 import axios from "axios"
+import { sleep } from "../utils/utils"
+
+type Price = {
+    tokenAddress: string,
+    tokenSymbol: string,
+    minPrice: string,
+    maxPrice: string,
+    updatedAt: number,
+    priceDecimals: number
+}
 
 export const getPrice = async () => {
-    const res = await axios.get("http://54.180.247.27:8082/prices/tickers");
-    const price = res.data;
-    console.log("price: ", price);
+
+    try {
+        while(true) {
+            const res = await axios.get("http://54.180.247.27:8080/prices/tickers")
+
+            if(!res.data) {
+                throw new Error("unable to fetch price data")
+            }
+
+            for(const price of res.data) {
+                priceData[price.tokenAddress] = price
+            }
+
+            await sleep(500)
+        }
+    } catch (error) {
+        console.error("error occured: ", error)
+    }
+    
 }
+
+export const priceData: { [key: string]: Price } = {}
